be7b68712d16667e6cac88cfb28e04d5a03eef1d
CintronAleyna-FS <88294142+CintronAleyna-FS@users.noreply.github.com>
2022-06-04 19:27:37 -0500

    routes and server up

---
+++ app/app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const userRoute = require('../api/routes/userRoute');
+require('dotenv').config();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Acess-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, GET, DELETE');
+    }
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Service is up',
+        method: req.method,
+    });
+});
+
+app.use('/users', userRoute);
+
+app.use((req, res, next) => {
+    const error = new Error('NOT FOUND!!!');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        error: {
+            message: error.message,
+            status: error.status,
+        },
+    });
+});
+
+// connect to mongodb
+mongoose.connect(process.env.mongoDBURL, (err) => {
+    if(err){
+        console.log("Error ", err.message);
+    }
+    else{
+        console.log('MongoDB connection was successful')
+    }
+})
+
+module.exports = app;
